3174b53fba4982c68d355f101c0b0263fbb4d4a6
Joanna Margareth Nauli <18220007@mahasiswa.itb.ac.id>
2023-02-08 17:45:07 -0500

    update hw 2

--- Image Playground.playground/Contents.swift
+++ Image Playground.playground/Contents.swift
@@ -6,12 +6,12 @@ var greeting = "Kombucha! 🍓"
 let dimension = CGSize(width: 1024, height: 1024)
 let renderer = UIGraphicsImageRenderer(size: dimension)
 
-let kombucha = UIImage(named: "Kombucha.jpg")
-let juice = UIImage(named: "Juice.jpg")
+let kombucha = UIImage(named: "Kombucha.jpeg")
+let juice = UIImage(named: "Juice.jpeg")
 
 let image = renderer.image { (context) in
     kombucha?.draw(in: CGRect(x: 0, y: 0, width: 1024, height: 1024))
-    juice?.draw(in: CGRect(x: 150, y: 0, width: 1024, height: 1024))
+    juice?.draw(in: CGRect(x: 320, y: 0, width: 1024, height: 1024))
 }
 
 image
